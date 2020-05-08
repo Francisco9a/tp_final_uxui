9eac8a10bf3b1cdd2288c1f2ef50201c1039b1eb
Francisco <francisco9altamirano@gmail.com>
2020-05-08 11:38:34 -0300

    primer subida Trabajo Final UX/UI

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//     $(".tarjeta-cuenta__radius-button").click(function () { 
+//             $(".tarjeta-cuenta__radius-button").toggle(); 
+
+//         }
+//         );
+
+// });
